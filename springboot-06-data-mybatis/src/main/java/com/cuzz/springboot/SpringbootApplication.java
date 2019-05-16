64c0364812db778cb93e6d2209a17f2b8a1eb7f6
huaxialongguanggithub <50685958+huaxialongguanggithub@users.noreply.github.com>
2019-05-16 15:43:38 +0800

    Add files via upload

---
+++ springboot-06-data-mybatis/src/main/java/com/cuzz/springboot/SpringbootApplication.java
@@ -0,0 +1,15 @@
+package com.cuzz.springboot;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@MapperScan(value = "com.cuzz.springboot.mapper")
+@SpringBootApplication
+public class SpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootApplication.class, args);
+	}
+}
